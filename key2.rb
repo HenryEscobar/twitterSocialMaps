3793c8a81bc2de2b3ae368df856480153f85b85f
Henry Escobar <henry.escobar@gmail.com>
2013-12-08 14:53:34 +0000

    Minor cleanup

---
+++ key2.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/ruby
+################################################################################
+#
+# Twitter key info and config
+#
+################################################################################
+
+Twitter.configure do |config|
+  config.consumer_key = 'XXXXXXXXXXXXXXXXXXXXXX'
+  config.consumer_secret = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+  config.oauth_token = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX' 
+  config.oauth_token_secret = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+end
+
+
+
